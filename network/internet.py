fa1eadeca396bec9edeb0be58d312a74139e3ffc
happys <happys2333@outlook.com>
2023-04-19 23:14:18 +0800

    add command

--- network/internet.py
+++ network/internet.py
@@ -1,15 +1,13 @@
 from mininet.net import Mininet
 from mininet.clean import Cleanup
-from .topo import internet_topo
+from topo import internet_topo
 from config import config
 from mininet.node import RemoteController
 
-
 topo = internet_topo['Star']()
-
-
-net = Mininet(topo=topo,controller=RemoteController(name='SUSTC ITSC',ip='',port=config.CONTROLLER_PORT))
-
+# TODO: change the ip address to your controller's ip address
+net = Mininet(topo=topo, controller=RemoteController(name='SUSTC ITSC', ip='**.**.**.**', port=config.CONTROLLER_PORT),
+              autoSetMacs=False)
 
 # clean up cache
 Cleanup()
--- network/intranet.py
+++ network/intranet.py
@@ -1,14 +1,15 @@
 from mininet.net import Mininet
 from mininet.clean import Cleanup
-from .topo import intranet_topo
+from topo import intranet_topo
 from config import config
 from mininet.node import RemoteController
 
 
 topo = intranet_topo['Single']()
 
-
-net = Mininet(topo=topo,controller=RemoteController(name='SUSTC ITSC',ip='',port=config.CONTROLLER_PORT))
+# TODO: change the ip address to your controller's ip address
+net = Mininet(topo=topo, controller=RemoteController(name='SUSTC ITSC', ip='127.0.0.1', port=config.CONTROLLER_PORT),
+              autoSetMacs=False)
 
 
 # clean up cache
